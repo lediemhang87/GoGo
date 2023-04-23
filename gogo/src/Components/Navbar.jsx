974b14f3b89b0a44e1e343796586c01417619efe
Ashley Le <53335012+lediemhang87@users.noreply.github.com>
2023-04-23 04:20:25 -0700

    Finishing up project

--- gogo/src/Components/Navbar.jsx
+++ gogo/src/Components/Navbar.jsx
@@ -13,14 +13,14 @@ const Navbar = () => {
       <div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div className='container'> 
-                <img src={require("../Styles/img/Gogo-logo.PNG")} alt="" width="80px" />
+                <a class="nav-link" href="/"><img src={require("../Styles/img/Gogo-logo.PNG")} alt="" width="80px" /></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul class="navbar-nav  ms-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="/">Home <span class="sr-only"></span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/planner">Planning</a>
